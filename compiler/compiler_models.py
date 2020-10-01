f7541d2e9287daf1f2fa49eeb66d025fd803cebb
Eliran Ziv <Eliran2001@gmail.com>
2020-10-01 03:02:58 +0300

    Finished parsing basic tokens

---
+++ compiler/compiler_models.py
@@ -0,0 +1,19 @@
+class NameMatch:
+    def __init__(self, value):
+        self.value = value
+
+
+class ConstantMatch:
+    def __init__(self, value):
+        self.value = value
+
+
+class StringMatch:
+    def __init__(self, value):
+        self.value = value
+
+
+class FunctionMatch:
+    def __init__(self, name, arguments):
+        self.arguments = arguments
+        self.name = name
