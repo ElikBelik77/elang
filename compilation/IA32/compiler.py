81139158e8c488b90e74bcf6832dddef0948b8c1
Eliran Ziv <Eliran2001@gmail.com>
2020-10-14 21:25:32 +0300

    Stack size fix. Utilizing the entire stack now. (Missed dword on ebp)

--- compilation/IA32/compiler.py
+++ compilation/IA32/compiler.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from compilation.IA32.template_factories import *
 
 
@@ -51,28 +53,24 @@ class ProgramCompiler:
         assembly = ("SECTION .text\n"
                     "global main\n")
         for function in program.functions:
-            offset_table = self.produce_offset_table(function)
-            assembly += self.compile_function(function, offset_table) + "\n"
+            assembly += self.compile_function(function) + "\n"
         with open(destination_file, "w") as out:
             out.write(assembly)
 
-    def compile_function(self, function, offset_table) -> str:
+    def compile_function(self, function) -> str:
         """
         This function compiles a single function.
         :param function: the function to compile.
         :param offset_table: the offset table of the function.
         :return: the assembly code of the function.
         """
-        if len(offset_table.keys()) == 0:
-            stack_size = 0
-        else:
-            stack_size = abs(min(offset_table.values()))
+        offset_table, stack_size = self.produce_offset_table(function)
         return self.factories[Function].produce(function, self.factories,
                                                 {"stack_size": stack_size, "offset_table": offset_table,
                                                  "parent": 'global', "primitive_bundle": self.primitive_bundle,
                                                  "verbose": self.verbose})
 
-    def produce_offset_table(self, scopeable: Scopeable) -> Dict[str, int]:
+    def produce_offset_table(self, scopeable: Scopeable) -> Tuple[Dict[str, int], int]:
         """
         This function produces an offset table for a scope.
         :param scopeable: the scope.
@@ -84,14 +82,14 @@ class ProgramCompiler:
             for idx, arg in enumerate(scopeable.arguments):
                 scope_table[arg.name] = 12 + idx * 4
         scopes: List[Scopeable] = [scopeable]
+        stack_size = 0
         while len(scopes) is not 0:
             current_scope = scopes.pop()
-            last_offset = 0
             for idx, key in enumerate(current_scope.scope.defined_variables):
-                scope_table[key] = last_offset
-                last_offset = scope_table[key] - current_scope.scope.defined_variables[key]["type"].get_size(
+                scope_table[key] = stack_size
+                stack_size = scope_table[key] - current_scope.scope.defined_variables[key]["type"].get_size(
                     self.primitive_bundle)
             for compilable in current_scope.body:
                 if issubclass(type(compilable), Scopeable):
                     scopes.append(compilable)
-        return scope_table
+        return scope_table, abs(stack_size)
--- tests/out/array_headers.asm
+++ tests/out/array_headers.asm
@@ -4,57 +4,57 @@ main:
 push ebp
 mov ebp, esp
 sub esp, 408
-mov edi, [ebp - 28]
-mov [edi - 0], 5
-mov [edi - 4], 4
-mov edi, [ebp - 176]
-mov [edi - 0], 5
-mov [edi - 4], 28
-mov [edi - 8], 5
-mov [edi - 12], 4
-mov [edi - 36], 5
-mov [edi - 40], 4
-mov [edi - 64], 5
-mov [edi - 68], 4
-mov [edi - 92], 5
-mov [edi - 96], 4
-mov [edi - 120], 5
-mov [edi - 124], 4
-mov edi, [ebp - 408]
-mov [edi - 0], 4
-mov [edi - 4], 56
-mov [edi - 8], 3
-mov [edi - 12], 16
-mov [edi - 64], 3
-mov [edi - 68], 16
-mov [edi - 120], 3
-mov [edi - 124], 16
-mov [edi - 176], 3
-mov [edi - 180], 16
-mov [edi - 16], 2
-mov [edi - 20], 4
-mov [edi - 32], 2
-mov [edi - 36], 4
-mov [edi - 48], 2
-mov [edi - 52], 4
-mov [edi - 72], 2
-mov [edi - 76], 4
-mov [edi - 88], 2
-mov [edi - 92], 4
-mov [edi - 104], 2
-mov [edi - 108], 4
-mov [edi - 128], 2
-mov [edi - 132], 4
-mov [edi - 144], 2
-mov [edi - 148], 4
-mov [edi - 160], 2
-mov [edi - 164], 4
-mov [edi - 184], 2
-mov [edi - 188], 4
-mov [edi - 200], 2
-mov [edi - 204], 4
-mov [edi - 216], 2
-mov [edi - 220], 4
+lea edi, [ebp - 0]
+mov [edi - 0], dword 5
+mov [edi - 4], dword 4
+lea edi, [ebp - 28]
+mov [edi - 0], dword 5
+mov [edi - 4], dword 28
+mov [edi - 8], dword 5
+mov [edi - 12], dword 4
+mov [edi - 36], dword 5
+mov [edi - 40], dword 4
+mov [edi - 64], dword 5
+mov [edi - 68], dword 4
+mov [edi - 92], dword 5
+mov [edi - 96], dword 4
+mov [edi - 120], dword 5
+mov [edi - 124], dword 4
+lea edi, [ebp - 176]
+mov [edi - 0], dword 4
+mov [edi - 4], dword 56
+mov [edi - 8], dword 3
+mov [edi - 12], dword 16
+mov [edi - 64], dword 3
+mov [edi - 68], dword 16
+mov [edi - 120], dword 3
+mov [edi - 124], dword 16
+mov [edi - 176], dword 3
+mov [edi - 180], dword 16
+mov [edi - 16], dword 2
+mov [edi - 20], dword 4
+mov [edi - 32], dword 2
+mov [edi - 36], dword 4
+mov [edi - 48], dword 2
+mov [edi - 52], dword 4
+mov [edi - 72], dword 2
+mov [edi - 76], dword 4
+mov [edi - 88], dword 2
+mov [edi - 92], dword 4
+mov [edi - 104], dword 2
+mov [edi - 108], dword 4
+mov [edi - 128], dword 2
+mov [edi - 132], dword 4
+mov [edi - 144], dword 2
+mov [edi - 148], dword 4
+mov [edi - 160], dword 2
+mov [edi - 164], dword 4
+mov [edi - 184], dword 2
+mov [edi - 188], dword 4
+mov [edi - 200], dword 2
+mov [edi - 204], dword 4
+mov [edi - 216], dword 2
+mov [edi - 220], dword 4
 leave
 ret
 
